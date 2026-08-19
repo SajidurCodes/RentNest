c74c8c84de487f8c11f9be1408f304c34f0b5cc2
Sajidur Rahman Siam <siamsajidur@gmail.com>
2026-08-20 01:47:12 +0600

    fixed cors

--- src/app.ts
+++ src/app.ts
@@ -14,21 +14,10 @@ import { paymentRoutes } from "./modules/payment/payment.route";
 
 const app: Application = express();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  config.app_url, 
-].filter(Boolean);
-
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
+    origin : config.app_url,
+    credentials : true,
+}))
 
 app.use("/api/payments/confirm", express.raw({ type: 'application/json' }))
 app.use(cookieParser());
